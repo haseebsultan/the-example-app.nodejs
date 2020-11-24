18192957d3aae317f0374bba9b2374e39f71204d
haseebsultan <38536290+haseebsultan@users.noreply.github.com>
2020-11-24 16:05:59 +0500

    Update contentful.js

--- services/contentful.js
+++ services/contentful.js
@@ -2,3 +2,7 @@
  * The purpose of this module is to get data from contentful
  */
 const { createClient } = require('contentful'
+                                 sdkvnkfnv
+                                 mvdfnbodkgnb
+                                 ds;knvfdnbegenb
+                                 sflnvfbnnbskfbnskf
